fff0a13b09fb58d10274bbb45ae0c6de0415e9d1
Oleksandr Ievtieiev <50827046+Sansanchik@users.noreply.github.com>
2019-12-19 14:02:28 +0200

    3
    3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
+ const windowHeight = $(window).height();
+$(window).on(`scroll`, function () {
+    if (pageYOffset >= windowHeight) {
+        $(`#btn-up`).css(`display`, `block`);
+    } else {
+        $(`#btn-up`).css(`display`, `none`);
+    }
+});
+$(`#btn-up`).on(`click`, function () {
+    event.preventDefault();
+    $('body,html').animate({'scrollTop': `0`}, 1000);
+});
+
    $('.multiple-items').slick({
       infinite: true,
        slidesToShow: 3,
       slidesToScroll: 1
     });
+
